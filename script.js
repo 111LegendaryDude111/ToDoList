e525ecc76bea47c7e05f07e2d12a60ce1413ae6e
David <zigered4@gmail.com>
2022-11-05 22:41:28 +0300

    optimize code and add delete button

--- script.js
+++ script.js
@@ -1,62 +1,123 @@
-window.onload = function(){
 
-
-   let toDoList = [];
-   // if( localStorage.getItem('todo') != undefined &&  localStorage.getItem('todo'))
-   if( localStorage.getItem('todo') != undefined ){
-      toDoList = JSON.parse(localStorage.getItem('todo'));
-      out();
-      console.log(typeof localStorage.getItem('todo'));
+window.addEventListener('DOMContentLoaded',()=> {
+   let targetArray;
+   if (localStorage.getItem('list')){
+     targetArray = JSON.parse(localStorage.getItem('list'));
+   }else{
+    targetArray = [];
    }
-   document.getElementById('add').onclick = function(){
-      let d = document.getElementById('in').value;
-      document.getElementById("in").value = "";
+   let btn = document.querySelector('#add'),
+      out = document.querySelector('#out');
+
+
+   btn.addEventListener('click', (e,i = 0) => {
+      let targets = document.querySelector('#in').value;
       let temp = {};
-      temp.todo = d;
-      temp.check = false;
-      temp.background = '';
-      let i = toDoList.length;
-      toDoList[i] = temp;
-      console.log(toDoList);
-      out();
-      checkCheckboxes();
-      localStorage.setItem('todo', JSON.stringify(toDoList));
-   };
+      temp.text = targets;
+      document.querySelector('#in').value = '';
+      targetArray.push(temp);
+      localStorage.setItem('list', JSON.stringify(targetArray));
+      createEl();
 
-function checkCheckboxes() {
-   let checkboxes = document.querySelectorAll('input[type=checkbox]');
+   });
 
-   for (let i = 0; i < checkboxes.length; i++) {
-       checkboxes[i].onchange = function () {
-           toDoList = JSON.parse(localStorage.getItem('todo'));
-           toDoList[i].check = this.checked;
-         //   this.style.color = 'green';
-         //   console.log(document.querySelectorAll(`label[for=input-${i}]`)[0].style.color = 'green');
-         document.querySelectorAll(`label[for=input-${i}]`)[0].style.background = 'green';
-         toDoList[i]["background"] = 'green';
-         console.log(toDoList[i]);
-           localStorage.setItem('todo', JSON.stringify(toDoList));
-       };
-   }
-}
-   function out() {
-      let out = '';
-      for ( let key in toDoList){
-         if( toDoList[key].check === true){
-            out += '<input id="input-'+ key + '" class="inputCheck" type="checkbox" checked> <label style="background:'+ toDoList[key].background+'" for="input-'+ key + '" > '+ toDoList[key].todo +'</label>';
-         }else {
-            out += '<input id="input-'+ key + '" class="inputCheck" type="checkbox"> <label style="background:'+ toDoList[key].background+'" for="input-'+ key + '" > '+ toDoList[key].todo +' </label>';
-         }
-         // out += toDoList[key].todo + '<br>';
-         out +=  '<br>';
+//добавления элементов на страницу
+   function createEl(){
+      let tempList = JSON.parse(localStorage.getItem('list'));
+      if (tempList){
+         out.innerHTML = '';
+            for (let i = 0; i < tempList.length;i++){
+               let div = document.createElement('div');
+               div.classList.add('div-target')
+               let input = document.createElement('input');
+               input.setAttribute('type','checkbox');
+               input.classList.add('checkbox');
+               let img = document.createElement('img');
+               img.src = 'close.png';
+               img.classList.add('close');
+               div.innerHTML += tempList[i].text;
+               div.prepend(input);
+               div.append(img);
+               out.append(div);
+            }
+      }else{
+         console.log('Hello,mate!');
       }
+
       
-      document.getElementById('out').innerHTML = out;
+      let divs = document.querySelectorAll('.div-target');
+      divs.forEach(item => {
+         for (let i = 0; i < tempList.length;i++){
+            if (item.innerText === tempList[i].text && tempList[i].state ){
+               item.classList.add('check');
+               item.childNodes[0].checked = 'checked';
+            }
 
+            }
+      });
+      inputEvent();
+      closeBtn();
+}
+createEl();
+
+  //добавления события на инпуты
+   function inputEvent(){
+      let inputs = document.querySelectorAll('.checkbox');
+      let tempList = JSON.parse(localStorage.getItem('list'));
+
+         inputs.forEach(input => input.addEventListener('change', (e)=>{
+            let target = e.target;
+            let parent = target.parentNode;
+            
+            if (parent.classList.contains('check')){
+                  parent.classList.remove('check');
+                  let j = tempList.findIndex(el => el.text === parent.innerText);
+                  delete tempList[j].state;
+            }else{
+               parent.classList.add('check');
+               let i = tempList.findIndex(el => el.text === parent.innerText);
+               tempList[i].state = 'done';
+            }
+            localStorage.setItem('list', JSON.stringify(tempList));
+         }));
    }
-};
 
-document.getElementById('clear').onclick = function(){
-   localStorage.removeItem('todo');
-   location.reload();
-};
+   //добавление события на картинки выдр удаления задачи
+   function closeBtn(){
+      let btn = document.querySelectorAll('.close');
+      let tempList = JSON.parse(localStorage.getItem('list'));
+      
+
+
+      btn.forEach(btn => btn.addEventListener('click', (e) => {
+         let target = e.target;
+         let parent = target.parentNode;
+         parent.style.display ='none';
+         let i = tempList.findIndex(el => el.text === parent.innerText);
+         delete tempList[i];
+         tempList = tempList.filter(el => typeof(el) !== 'undefined');
+         localStorage.setItem('list', JSON.stringify(tempList));
+      }));
+   }
+
+//конпка очистки
+
+   let clearBtn = document.querySelector('#clear');
+   clearBtn.addEventListener('click', ()=>{
+      localStorage.clear();
+      location.reload();
+   });
+
+   let fjBtn = document.querySelector('#fj');
+   fjBtn.addEventListener('click', () => {
+      if (fjBtn.classList.contains('yellow')){
+         document.body.style.backgroundColor = 'pink';
+         fjBtn.classList.add('pink');
+         fjBtn.classList.remove('yellow');
+      }else if (fjBtn.classList.contains('pink')) {
+         document.body.style.backgroundColor = 'yellow';
+         fjBtn.classList.remove('pink');
+         fjBtn.classList.add('yellow');
+      }
+   });
+});
